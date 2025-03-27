35aa3728f5c8e4f4927722a5edf5b1e53813f097
Dexz69 <lutgardezboclog789@gmail.com>
2025-03-27 22:04:44 +0800

    Update exercise1.php

---
+++ exercise1.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 15;
+    $b = 7;
+
+    echo "Sum: " . ($a + $b) . "<br>";
+    echo "Difference: " . ($a - $b) . "<br>";
+    echo "Product: " . ($a * $b) . "<br>";
+    echo "Quotient: " . ($a / $b) . "<br>";
+?>
